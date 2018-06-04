c8c5112f391e191372197c513b8e7c2939876e02
Brandon <brandoncyu@gmail.com>
2018-06-04 12:56:42 -0700

    added attribute button, started saving things to local storage

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,45 @@
-const create = document.querySelector('#create')
+const adding = document.getElementById('add')
 
-create.addEventListener('submit', (event) => {
+adding.addEventListener('click', (event)=>{
+  event.preventDefault()
+  let bigWhat = document.querySelector('#big-what')
+  let newInput = document.createElement('input')
+  newInput.setAttribute('placeholder', "Something interesting about this person")
+  let space = document.createElement('br')
+  bigWhat.appendChild(newInput)
+  bigWhat.appendChild(space)
+})
+
+const create = document.getElementById('create')
+
+create.addEventListener('submit', (event)=>{
   event.preventDefault()
 
   const firstName = document.querySelector('#firstname').value
   const lastName = document.querySelector('#lastname').value
   const where = document.querySelector('#where').value
-  const when = document.querySelector('#when').value
-  const what1 = document.querySelector('#what1').value
-  const what2 = document.querySelector('#what2').value
-  const what3 = document.querySelector('#what3').value
+  const when = document.querySelector('#month').value + '-' + document.querySelector('#day').value + '-' + document.querySelector('#year').value
+  let what = []
+  const whatQueries = document.querySelectorAll('#big-what input')
+  for (i = 0; i < whatQueries.length; i++){
+    if (whatQueries[i].value){
+      what.push(whatQueries[i].value)
+    }
+  }
 
-  console.log(firstName)
+  let exportedValues = {
+    'firstName': firstName,
+    'lastName': lastName,
+    'where': where,
+    'when': when,
+    'what': what
+  }
+  let importedData = JSON.parse(localStorage.getItem('cards'))
+  if (importedData == null) importedData = []
+  console.log(importedData)
+  importedData.push(exportedValues)
+  console.log(importedData)
+  localStorage.setItem('cards', JSON.stringify(importedData))
+
+  document.getElementById('create').reset();
 })
